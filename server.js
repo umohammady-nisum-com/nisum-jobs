0860f862bdf4c9fe6db26f1a6c5b1eea9b2ddb22
Umair Mohammady <Umair@Umairs-MacBook-Pro.local>
2017-09-18 16:20:37 -0700

    changes to db made

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Start the DB
-app.connect(process.env.MONGODB_URI);
+app.connect('mongodb://heroku_5vgn0sv7:h10heeas7lpoc2j1vj06p85gjt@ds135514.mlab.com:35514/heroku_5vgn0sv7');
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
